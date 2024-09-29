081b8ae9ba161b19737056e063c8210f7e09d49a
KutayOzkoc <kutayozkoc99@gmail.com>
2024-09-29 11:21:51 +0300

    db connection

--- bankingappwdjango/bankingappwdjango/settings.py
+++ bankingappwdjango/bankingappwdjango/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'bankingappwdjango.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',  # PostgreSQL database name
+        'USER': 'postgres',       # PostgreSQL username
+        'PASSWORD': '123456',    # PostgreSQL password
+        'HOST': 'localhost',      # Usually localhost
+        'PORT': '5432',           # Default PostgreSQL port
     }
 }
 
